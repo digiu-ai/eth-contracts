fa8abe0b618f0e66343e915fc19df08b89f9d9ba
v0ld <v0ld@v0ld.com>
2021-06-18 20:36:54 +0700

    dexpool for testnet

--- truffle/migrations/2_deploy_testnet.js
+++ truffle/migrations/2_deploy_testnet.js
@@ -2,6 +2,7 @@ const { exec } = require('child_process');
 
 const NodeList                          = artifacts.require('NodeList');
 const Bridge                            = artifacts.require('Bridge');
+const MockDexPool                       = artifacts.require('MockDexPool');
 
 module.exports = async (deployer, network, accounts) => {
 
@@ -21,9 +22,13 @@ if(network === 'rinkeby' || network === 'bsctestnet'){
                                         await deployer.deploy(Bridge, nodeList.address, { from: owner });
               let bridge              = await Bridge.deployed();
 
+                                        await deployer.deploy(MockDexPool, bridge.address, { from: owner });
+              let mockDexPool         = await MockDexPool.deployed();              
+                                        await bridge.updateDexBind(mockDexPool.address, true, {from: owner});
+
               
               let env_file = `env_connect_to_${_network}.env`;
-              exec(`${process.cwd()}/scripts/bash/update_env_adapter.sh 8081 ${_network.toUpperCase()} ${bridge.address} ${env_file} ${nodeList.address}  ""`, { maxBuffer: 1024 * 100000000 }, (err, stdout, stderr) => {
+              exec(`${process.cwd()}/scripts/bash/update_env_adapter.sh 8081 ${_network.toUpperCase()} ${bridge.address} ${env_file} ${nodeList.address}  ${mockDexPool.address} `, { maxBuffer: 1024 * 100000000 }, (err, stdout, stderr) => {
                 if (err) {
                     console.log('THROW ERROR', err);
                     return;
