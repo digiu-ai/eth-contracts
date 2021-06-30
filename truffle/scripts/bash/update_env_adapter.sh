0351bab51037448f2ad98bbae83c762f55cefee7
Dmitriy Khristianov <support@dkh.ru>
2021-06-30 15:58:34 +0300

    move to yaml config

--- truffle/scripts/bash/update_env_adapter.sh
+++ truffle/scripts/bash/update_env_adapter.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
-echo "
-BRIDGE_${2:-\"null\"}=${3:-\"null\"}
-NODELIST_${2:-\"null\"}=${5:-\"null\"}
-DEXPOOL_${2:-\"null\"}=${6:-\"null\"}"  > $4
+#echo "
+#BRIDGE_${2:-\"null\"}=${3:-\"null\"}
+#NODELIST_${2:-\"null\"}=${5:-\"null\"}
+#DEXPOOL_${2:-\"null\"}=${6:-\"null\"}"  > $4
 
+echo "
+RPC_URL=${1:-\"null\"}
+NETWORK_ID=${2:-\"null\"}
+BRIDGE_ADDRESS=${3:-\"null\"}
+NODELIST_ADDRESS=${4:-\"null\"}
+DEXPOOL_ADDRESS=${5:-\"null\"}"  > $6
