9a50c786e5ef88618a1010dbdd09289ea94f4d15
v0ld <v0ld@v0ld.com>
2021-07-08 11:28:36 +0700

    e2e synthesis under dev

--- truffle/migrations/2_deploy_testnet.js
+++ truffle/migrations/2_deploy_testnet.js
@@ -1,13 +1,15 @@
-const argv = require('minimist')(process.argv.slice(2), {string: ['bridge', 'nodelist', 'mockdexpool' ]});
+const argv = require('minimist')(process.argv.slice(2), {string: ['bridge', 'nodelist', 'mockdexpool', 'paymastergsn' ]});
 const { exec } = require('child_process');
 
 const NodeList                          = artifacts.require('NodeList');
 const Bridge                            = artifacts.require('Bridge');
 const MockDexPool                       = artifacts.require('MockDexPool');
 
-
 const Portal                            = artifacts.require('Portal');
 const Synthesis                         = artifacts.require('Synthesis');
+
+const TokenPaymasterPermitPaymaster     = artifacts.require('TokenPaymasterPermitPaymaster');
+const RelayHub                          = artifacts.require('RelayHub');
 
 
 module.exports = async (deployer, network, accounts) => {
@@ -16,12 +18,30 @@ module.exports = async (deployer, network, accounts) => {
   if (network === 'rinkeby')     _network = 'network1';
   if (network === 'bsctestnet')  _network = 'network2';
 
-const forwarder = network === 'rinkeby' ? "0x83A54884bE4657706785D7309cf46B58FE5f6e8a"  : network === 'bsctestnet' ? '0xeB230bF62267E94e657b5cbE74bdcea78EB3a5AB' : undefined  // it is real openGSN trustedForwarder
 
 if(network === 'rinkeby' || network === 'bsctestnet'){
      try {
 
-              const [owner, anotherAccount] = accounts;
+          const [owner, anotherAccount] = accounts;
+
+
+		  const relayHub  = network === 'rinkeby' ? '0x6650d69225CA31049DB7Bd210aE4671c0B1ca132' : network === 'bsctestnet' ? '0xAa3E82b4c4093b4bA13Cb5714382C99ADBf750cA' : undefined  // it is real openGSN addresess
+		  const forwarder = network === 'rinkeby' ? '0x83A54884bE4657706785D7309cf46B58FE5f6e8a' : network === 'bsctestnet' ? '0xeB230bF62267E94e657b5cbE74bdcea78EB3a5AB' : undefined  // it is real openGSN trustedForwarder
+		  const token     = network === 'rinkeby' ? "0x8aAFC440A5057cF8728c1C23fd74C25314c156ac" : network === 'bsctestnet' ? '0x8aAFC440A5057cF8728c1C23fd74C25314c156ac' : undefined;  // token should be in pool (uniswap, pancake and so >
+		  const router    = network === 'rinkeby' ? "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D" : network === 'bsctestnet' ? '0x8aAFC440A5057cF8728c1C23fd74C25314c156ac' : undefined;  // pool for certain network (uniswap, pancake and so>
+
+		if(network !== 'network1' && network !== 'network2' && argv.paymastergsn === void 0){
+
+		                                             await deployer.deploy(TokenPaymasterPermitPaymaster, { from: owner });
+		            this.paymaster                 = await TokenPaymasterPermitPaymaster.deployed();
+		                                             await this.paymaster.setRelayHub(relayHub);
+		                                             await this.paymaster.setTrustedForwarder(forwarder);
+		                                             //await paymaster.addToken(token, uniswap); //TODO more flexible.
+		            this.relay                     = await RelayHub.at(relayHub);
+		                                             //await relay.depositFor(paymaster.address, toWei(2)); // funds paymaster for initial start
+		}
+
+		this.paymaster = this.paymaster  === void 0 ? await TokenPaymasterPermitPaymaster.at(argv.paymastergsn) : this.paymaster;
 
           if(argv.bridge === undefined){
 
@@ -43,18 +63,18 @@ if(network === 'rinkeby' || network === 'bsctestnet'){
 
                                         await deployer.deploy(Portal, this.bridge.address, forwarder, { from: owner });
               let portal              = await Portal.deployed();
-                                        await this.bridge.updateDexBind(portal.address, true, {from: owner});
+                                        //await this.bridge.updateDexBind(portal.address, true, {from: owner});
 
                                         await deployer.deploy(Synthesis, this.bridge.address, forwarder, { from: owner });
               let synthesis           = await Synthesis.deployed();
-                                        await this.bridge.updateDexBind(synthesis.address, true, {from: owner});
+                                        //await this.bridge.updateDexBind(synthesis.address, true, {from: owner});
 
 					//await synthesis.createRepresentation(realALP888, "sALP888", "sALP888")
 
 
               let env_file = `env_connect_to_${_network}.env`;
               let n = _network.toUpperCase();
-              exec(`${process.cwd()}/scripts/bash/update_env_adapter.sh create ${env_file} BRIDGE_${n}=${this.bridge.address} NODELIST_${n}=${this.nodeList.address} DEXPOOL_${n}=${this.mockDexPool.address} PORTAL_${n}=${portal.address} SYNTHESIS_${n}=${synthesis.address}`, { maxBuffer: 1024 * 100000000 }, (err, stdout, stderr) => {
+              exec(`${process.cwd()}/scripts/bash/update_env_adapter.sh create ${env_file} BRIDGE_${n}=${this.bridge.address} NODELIST_${n}=${this.nodeList.address} DEXPOOL_${n}=${this.mockDexPool.address} PORTAL_${n}=${portal.address} SYNTHESIS_${n}=${synthesis.address} PAYMASTER_${n}=${this.paymaster.address}`, { maxBuffer: 1024 * 100000000 }, (err, stdout, stderr) => {
                 if (err) {
                     console.log('THROW ERROR', err);
                     return;
--- truffle/scripts/bash/deploy_testnet.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-npx truffle migrate --reset --network rinkeby    --bridge 0x58182e36006a201C8C3f02B2010ba9EE2863626d --nodelist 0xac1648149d10EBA6546De513010138012b503e72 --mockdexpool 0xac1648149d10EBA6546De513010138012b503e72
-npx truffle migrate --reset --network bsctestnet --bridge 0x3486a8889D92768a078feaB12608ff3a6e186AC3 --nodelist 0xb69787dc29FbeFa2aD7F03cf4d3BDd81a48BdbBF --mockdexpool 0xb69787dc29FbeFa2aD7F03cf4d3BDd81a48BdbBF
